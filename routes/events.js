0677f42279d46e4f4d9272fb6f445d79a0df7788
SERGIOMENACHO <82847158+SERGIOMENACHO@users.noreply.github.com>
2024-06-01 11:46:48 -0600

    Week 06 assignment

--- routes/events.js
+++ routes/events.js
@@ -1,14 +1,23 @@
 const routes = require("express").Router();
 const eventsController = require("../controllers/events");
 
+// require the validation 
+const validation = require("../middleware/validation");
+
 routes.get("/events", eventsController.getAllEvents);
 
 routes.get("/events/:id", eventsController.getOneEvent);
 
-routes.post("/events/create", eventsController.createEvent);
+routes.post(
+  "/events/create",
+  validation.saveEvents,
+  eventsController.createEvent
+);
 
-routes.put("/events/:id", eventsController.updateEvent);
+// Week 06 assignment
+routes.put("/events/:id", validation.saveEvents, eventsController.updateEvent);
 
+// Week 06 assignment
 routes.delete("/events/:id", eventsController.deleteEvent);
 
 module.exports = routes;
